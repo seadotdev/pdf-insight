1968fb75af88cc8b454f810c3330b9de105e8184
vladimirs <vladimirs@sea.dev>
2023-09-27 23:18:26 +0100

    clean up kg seed script

--- backend/scripts/seed_db.py
+++ backend/scripts/seed_db.py
@@ -16,6 +16,7 @@ from llama_index import (
     LLMPredictor,
     ServiceContext,
     KnowledgeGraphIndex,
+    load_index_from_storage,
 )
 from llama_index.graph_stores import SimpleGraphStore
 from llama_index.node_parser import SimpleNodeParser
@@ -41,44 +42,60 @@ def copy_to_s3(dir_path: str, s3_bucket: str = settings.S3_ASSET_BUCKET_NAME):
 
 
 async def async_seed_db():
-    path = DEFAULT_SOURCE_DIR
     s3_bucket = settings.S3_ASSET_BUCKET_NAME
     persist_dir = f"{settings.S3_BUCKET_NAME}"
+    fs = get_s3_fs()
 
+    path = DEFAULT_SOURCE_DIR
     print("Copying documents to S3")
     copy_to_s3(path, s3_bucket)
 
-    fs = get_s3_fs()
-
     print("Upserting records of pdf files into database")
-    all_pdf_docs = [os.path.join(path, f) for f in os.listdir(path) if not f.startswith('.') and f.endswith('.pdf')]
-    # TODO: for now it lives locally, but we want to move them to s3 buckets or equivalent 
+    all_pdf_docs = [os.path.join(path, f) for f in os.listdir(
+        path) if not f.startswith('.') and f.endswith('.pdf')]
+    # TODO: for now it lives locally, but we want to move them to s3 buckets or equivalent
     # can't do yet due to the url hack and the api endpoint relying on it (original design legacy)
     # fs.glob(f"{s3_bucket}/*.pdf")
 
     print("Indexing documents:\n")
     print(all_pdf_docs)
 
-    kg_docs = []
     print("Seeding storage context")
     for filename in all_pdf_docs:
         print(f"Processing {filename}...\n")
 
         # Placeholder metadata, need a more robust way of populating this stuff
-        metadata = {"name": "Good Company Inc", "doc_type": DocumentTypeEnum.ANNUAL_REPORT, "year": 2022}
+        metadata = {"name": "Good Company Inc",
+                    "doc_type": DocumentTypeEnum.ANNUAL_REPORT, "year": 2022}
         doc = await upsert_document.upsert_single_document(url=filename, metadata=metadata)
 
-        # This is one of the SMEC documents, use this for now to generate KG
-        if(filename == "data/12592114_aa_2023-08-04.pdf"):
-            kg_docs = kg_docs + fetch_and_read_document(doc)
+        # This is one of the SMEC documents, use this for now to generate KG, if we want to use any docs to initialize
+        # if(filename == "data/12592114_aa_2023-08-04.pdf"):
+        #     kg_docs = kg_docs + fetch_and_read_document(doc)
 
         # Build index for the document
         service_context = get_tool_service_context([])
         await build_doc_id_to_index_map(service_context, [doc], fs=fs)
 
-    # Build the KG here        
+    print(
+        """
+Done! 🏁
+\t- PDF documents uploaded to the S3 assets bucket ✅
+\t- Documents database table has been populated ✅
+\t- Vector storage table has been seeded with embeddings ✅
+\t- Knowledge graph index has been generated
+    """.strip()
+    )
+
+
+def build_kg():
+    s3_bucket = settings.S3_ASSET_BUCKET_NAME
+    persist_dir = f"{settings.S3_BUCKET_NAME}"
+    fs = get_s3_fs()
+
+    # Build the KG here
     print("Generating Knowledge Graph...\n")
-    
+
     graph_store = SimpleGraphStore()
     storage_context = StorageContext.from_defaults(graph_store=graph_store)
 
@@ -94,8 +111,9 @@ async def async_seed_db():
     # )
     service_context = ServiceContext.from_defaults(llm=llm, chunk_size=512)
 
-    # Building KG index automatically from the documents (can override with json input or seed with some custom data)
+    # Building an empty KG (can override with json input or seed with some custom data)
     # KnowledgeGrapIndex will also build a vectorstore alongside the graphstore by default
+    kg_docs = []
     kg_index = KnowledgeGraphIndex.from_documents(
         kg_docs,
         max_triplets_per_chunk=15,
@@ -108,19 +126,37 @@ async def async_seed_db():
     print(f"Index Id: {kg_index.index_id}")
 
     # Here we can stick in whatever triplets we like, e.g.
-
     # kg_index.upsert_triplet(("James Kaberry", "is director of", "SME ANALYTICS & TECHNOLOGIES LIMITED"))
 
+    # Store the index in the s3 bucket
     kg_index.storage_context.persist(persist_dir=persist_dir, fs=fs)
 
-    print(
-        """
-Done! 🏁
-\t- PDF documents uploaded to the S3 assets bucket ✅
-\t- Documents database table has been populated ✅
-\t- Vector storage table has been seeded with embeddings ✅
-\t- Knowledge graph index has been generated
-    """.strip()
+
+def load_kg(index_id: str):
+    # Loading the index
+    s3_fs = get_s3_fs()
+    persist_dir = f"{settings.S3_BUCKET_NAME}"
+    storage_context = StorageContext.from_defaults(
+        fs=s3_fs, persist_dir=persist_dir)
+
+    # Rebel supports up to 512 input tokens, but shorter sequences also work well
+    llm = OpenAI(model="gpt-4", temperature=0)
+
+    # Can customize various parameters of the service context
+    # service_context = ServiceContext.from_defaults(
+    #   llm=llm,
+    #   embed_model=embed_model,
+    #   node_parser=node_parser,
+    #   prompt_helper=prompt_helper
+    # )
+
+    service_context = ServiceContext.from_defaults(llm=llm, chunk_size=512)
+    kg_index = load_index_from_storage(
+        storage_context=storage_context,
+        index_id=index_id,
+        service_context=service_context,
+        max_triplets_per_chunk=15,
+        verbose=True,
     )
 
 
@@ -129,4 +165,5 @@ def seed_db():
 
 
 if __name__ == "__main__":
-    Fire(seed_db)
+    # Fire(load_kg("bdb5f7e9-62a7-453b-8645-01e4e7757bc9"))
+    Fire(build_kg)
