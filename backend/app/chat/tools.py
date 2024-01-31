34563c75e886ab8ce3c661f086e09e80469ccebd
vladimirs <vladimirs@sea.dev>
2024-01-31 13:06:45 +0000

    clean up to remove stuff

--- backend/app/chat/tools.py
+++ backend/app/chat/tools.py
@@ -18,15 +18,6 @@ logger = logging.getLogger(__name__)
 
 
 def build_title_for_document(document: DocumentSchema) -> str:
-    # sec_metadata = DocumentMetadata.parse_obj(
-    #     document.metadata_map[DocumentMetadataKeysEnum.SEC_DOCUMENT]
-    # )
-    # time_period = (
-    #     f"{sec_metadata.year} Q{sec_metadata.quarter}"
-    #     if sec_metadata.quarter is not None
-    #     else str(sec_metadata.year)
-    # )
-    # return f"{sec_metadata.company_name} ({sec_metadata.company_ticker}) {sec_metadata.doc_type.value} ({time_period})"
     return ("Company Metadata")
 
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,31 +1,14 @@
-from typing import cast
 import uvicorn
 import logging
 import sys
-import sentry_sdk
 from starlette.middleware.cors import CORSMiddleware
-from alembic.config import Config
-from alembic import script
-from alembic.runtime import migration
-from sqlalchemy.engine import create_engine, Engine
-from llama_index.text_splitter.utils import split_by_sentence_tokenizer
 from app.api.api import api_router
 from app.core.config import settings, AppEnvironment
 from app.loader_io import loader_io_router
-from contextlib import asynccontextmanager
-from app.chat.pg_vector import get_vector_store_singleton, CustomPGVectorStore
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 logger = logging.getLogger(__name__)
-
-
-def check_current_head(alembic_cfg: Config, connectable: Engine) -> bool:
-    return True
-    # directory = script.ScriptDirectory.from_config(alembic_cfg)
-    # with connectable.begin() as connection:
-    #     context = migration.MigrationContext.configure(connection)
-    #     return set(context.get_current_heads()) == set(directory.get_heads())
 
 
 def __setup_logging(log_level: str):
@@ -42,55 +25,9 @@ def __setup_logging(log_level: str):
     logger.info("Set up logging with log level %s", log_level)
 
 
-def __setup_sentry():
-    # if settings.SENTRY_DSN:
-    #     logger.info("Setting up Sentry")
-    #     if settings.ENVIRONMENT == AppEnvironment.PRODUCTION:
-    #         profiles_sample_rate = None
-    #     else:
-    #         profiles_sample_rate = settings.SENTRY_SAMPLE_RATE
-    #     sentry_sdk.init(
-    #         dsn=settings.SENTRY_DSN,
-    #         environment=settings.ENVIRONMENT.value,
-    #         release=settings.RENDER_GIT_COMMIT,
-    #         debug=settings.VERBOSE,
-    #         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
-    #         profiles_sample_rate=profiles_sample_rate,
-    #     )
-    # else:
-    logger.info("Skipping Sentry setup")
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    return
-    # cfg = Config("alembic.ini")
-    # # Change DB URL to use psycopg2 driver for this specific check
-    # db_url = settings.DATABASE_URL.replace(
-    #     "postgresql+asyncpg://", "postgresql+psycopg2://"
-    # )
-    # cfg.set_main_option("sqlalchemy.url", db_url)
-    # engine = create_engine(db_url, echo=True)
-    # if not check_current_head(cfg, engine):
-    #     raise Exception(
-    #         "Database is not up to date. Please run `poetry run alembic upgrade head`"
-    #     )
-    # initialize pg vector store singleton
-    # vector_store = await get_vector_store_singleton()
-    # vector_store = cast(CustomPGVectorStore, vector_store)
-    # await vector_store.run_setup()
-
-    # # Some setup is required to initialize the llama-index sentence splitter
-    # split_by_sentence_tokenizer()
-    # yield
-    # # This section is run on app shutdown
-    # await vector_store.close()
-
-
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_PREFIX}/openapi.json",
-    # lifespan=lifespan,
 )
 
 
@@ -110,24 +47,13 @@ app.mount(f"/{settings.LOADER_IO_VERIFICATION_STR}", loader_io_router)
 
 def start():
     print("Running in AppEnvironment: " + settings.ENVIRONMENT.value)
-    # __setup_logging(settings.LOG_LEVEL)
-    # __setup_sentry()
-    """Launched with `poetry run start` at root level"""
-    # if settings.RENDER:
-    #     # on render.com deployments, run migrations
-    #     logger.debug("Running migrations")
-    #     alembic_args = ["--raiseerr", "upgrade", "head"]
-    #     alembic.config.main(argv=alembic_args)
-    #     logger.debug("Migrations complete")
-    # else:
-    logger.debug("Skipping migrations")
+    __setup_logging(settings.LOG_LEVEL)
         
-    live_reload = not settings.RENDER
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=live_reload,
+        reload=False,
         workers=settings.UVICORN_WORKER_COUNT,
     )
 
